054edcbb53d0a1c998174433a7950b20e3105d1c
Fred <e1375485@u.nus.edu>
2026-03-10 23:44:01 +0800

    Attempt tutorial 1

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,36 @@
+package seedu.address.model.person;
+
+public class Remark {
+
+    public final String value;
+
+    public Remark(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        Remark otherRemark = (Remark) other;
+        return value.equals(otherRemark.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
