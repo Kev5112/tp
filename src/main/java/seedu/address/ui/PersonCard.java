3e1b499081c6e13e3011d29f1eacd647c1667f0b
Hsu Myat Noe <hsumyatnoe134@gmail.com>
2026-03-10 12:52:03 +0800

    Add task index labeling to PersonCard UI

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -1,6 +1,7 @@
 package seedu.address.ui;
 
-import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -8,6 +9,7 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
+import seedu.address.model.task.Task;
 
 /**
  * An UI component that displays information of a {@code Person}.
@@ -52,8 +54,10 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
-        person.getTasks().stream()
-                .sorted(Comparator.comparing(task -> task.description))
-                .forEach(task -> tags.getChildren().add(new Label(task.description)));
+        List<Task> tasks = person.getTasks();
+        IntStream.range(0, tasks.size())
+                .forEach(i -> tags.getChildren().add(
+                        new Label((i + 1) + ". " + tasks.get(i).description)
+                ));
     }
 }
